a12a869f93395d204c823d210aeea2d02b561fb9
DanSnow <dododavid006@gmail.com>
2021-08-12 22:23:50 +0800

    chore: use yarn v3; update script

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,12 @@
-const {readFileSync, writeFileSync} = require('fs')
-const {cp, rm, mkdir, exec} = require('shelljs')
+const { readFileSync, writeFileSync } = require('fs')
+const { cp, rm, mkdir, exec } = require('shelljs')
 
 rm('-rf', 'dist')
-mkdir('dist')
+mkdir('-p', 'dist')
 cp('*.js', 'dist')
 const pkg = JSON.parse(readFileSync('package.json', 'utf8'))
 delete pkg.private
 delete pkg.devDependencies
-writeFileSync('dist/package.json', JSON.stringify(pkg, null, 2))
-exec('go build -o dist/parser.wasm', {env: {...process.env, GOOS: 'js', GOARCH: 'wasm'}})
+delete pkg.scripts
+writeFileSync('./dist/package.json', JSON.stringify(pkg, null, 2))
+exec('go build -o dist/parser.wasm', { env: { ...process.env, GOOS: 'js', GOARCH: 'wasm' } })
