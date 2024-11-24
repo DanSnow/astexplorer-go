56eb3115e268cc8d3ce4bff4c9c71449dfff56f7
DanSnow <dododavid006@gmail.com>
2024-11-24 17:19:27 +0800

    test: add test for wasm file

--- scripts/prepare-test.mts
+++ scripts/prepare-test.mts
@@ -2,3 +2,4 @@ import { fs, $ } from 'zx'
 
 await fs.remove('astexplorer-go')
 await $`go build -o astexplorer-go`
+await $`yarn run build`
--- tests/parser.spec.ts
+++ tests/parser.spec.ts
@@ -1,5 +1,12 @@
 import { beforeAll, expect, it } from 'vitest'
-import { $ } from 'zx'
+import { $, fs } from 'zx'
+import '../wasm_exec.js'
+
+declare global {
+  interface Window {
+    __GO_PARSE_FILE__: (content: string) => string
+  }
+}
 
 beforeAll(async () => {
   await $`tsx scripts/prepare-test.mts`
@@ -9,4 +16,14 @@ it('can parse generic', async () => {
   const { stdout } = await $`./astexplorer-go tests/examples/generic.go`
   expect(stdout).toContain('SumIntsOrFloats')
   expect(JSON.parse(stdout)).toMatchSnapshot()
+})
+
+it('can parse with wasm', async () => {
+  const go = new Go()
+  const resolvedUrl = (await import('../dist/parser.wasm?url')).default
+  const buffer = await fs.readFile('.' + resolvedUrl)
+  const { instance } = await WebAssembly.instantiate(buffer, go.importObject)
+  go.run(instance)
+  const content = await fs.readFile('tests/examples/generic.go', 'utf-8')
+  expect(JSON.parse(window.__GO_PARSE_FILE__(content))).toMatchSnapshot()
 })
